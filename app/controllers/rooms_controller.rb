1603f6d3467df5f612960ee1019dcea19ad54c79
vagrant <takakoba0426@gmail.com>
2020-05-30 05:08:26 +0000

    add DM

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,19 +3,24 @@ class RoomsController < ApplicationController
   end
 
   def show
+  	@room = Room.find(params[:id])
+
+          @message = Message.new
+          @entries = @room.entries
+          @rmember = Requester.find_by(@entries)
+          @umember = User.find_by(@entries)
+          @messages = Message.where(@entries)
   end
 
   def create
-  	@room = Room.new(room_params)
-    @room.user_id = params[:user_id].to_i
-    @room.requester_id = current_requester.id
-    @room.entry_id = Entry.find_by(user_id: params[:user_id],requester_id: current_requester.id,room_id: @room.id).id
+  	@room = Room.new
+    @room.entry_id = params.require(:entry).permit(:user_id, :room_id).merge(:room_id => @room.id,:requester_id => current_requester.id)
     @room.save
-      redirect_to room_path(@room.id)
+    redirect_to room_path(@room.id)
   end
 
 private
-  def room_params
-    params.require(:room).permit(:message)
+  def message_params
+    params.require(:message).permit(:mail)
   end
 end
