5342b33ae318791ad8efc491b1d6a72a26b97900
vagrant <takakoba0426@gmail.com>
2020-06-13 09:31:19 +0000

    [FIX]migrate fix

--- db/migrate/20200606024226_add_column_notification2.rb
+++ db/migrate/20200606024226_add_column_notification2.rb
@@ -1,8 +1,8 @@
 class AddColumnNotification2 < ActiveRecord::Migration[5.2]
   def change
-  	add_reference :notifications, :requester_visitor, foreign_key: true
-  	add_reference :notifications, :requester_visited, foreign_key: true
-  	add_reference :notifications, :company_visitor, foreign_key: true
-  	add_reference :notifications, :company_visited, foreign_key: true
+  	add_reference :notifications, :requester_visitor, foreign_key: { to_table: :requesters }
+  	add_reference :notifications, :requester_visited, foreign_key: { to_table: :requesters }
+  	add_reference :notifications, :company_visitor, foreign_key: { to_table: :companies }
+  	add_reference :notifications, :company_visited, foreign_key: { to_table: :companies }
   end
 end
